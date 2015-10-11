89d4c72ba37f85acb23ea9ff0324c9e63064b034
Caitlin Ross <rossc3@rpi.edu>
2015-10-11 14:23:31 -0400

    created Node and Appointment classes

---
+++ src/Appointment.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Caitlin Ross and Erika Mackin
+ *
+ */
+public class Appointment {
+
+	/**
+	 * 
+	 */
+	public Appointment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Node.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Caitlin Ross and Erika Mackin
+ *
+ */
+public class Node {
+
+	/**
+	 * 
+	 */
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
